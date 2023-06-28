c8918b17a52676605430fdba6c593bc872b7553c
Scarlettszsz <125333428+Scarlettszsz@users.noreply.github.com>
2023-06-28 18:24:06 +0900

    ksw
    20230628
    * 라이브러리 추가 및 버전 변경
    * mobile main section 작업
    * 그 외 기타 css

--- assets/js/examples.js
+++ assets/js/examples.js
@@ -1,4 +1,24 @@
-var myFullpage = new fullpage("#fullpage", {
-  verticalCentered: true,
-  sectionsColor: ["#1bbc9b", "#4BBFC3", "#7BAABE"],
+// fullpabe option
+$(document).ready(function () {
+  $("#fullpage").fullpage({
+    verticalCentered: true,
+    sectionsColor: ["#1bbc9b", "#4BBFC3", "#7BAABE"],
+  });
+});
+
+// section_01
+var swiper = new Swiper(".listSwiper", {
+  slidesPerView: 3,
+  spaceBetween: 0, // 슬라이드 여백
+  centeredSlides: true, // 슬라이드 중앙정렬
+  // initialSlide: 1, //초기 슬라이드 색인 번호
+  loop: true, // 무한반복
+  autoplay: {
+    delay: 2000,
+    // disableOnInteraction: true // 쓸어 넘기거나 버튼 클릭 시 자동 슬라이드 정지
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
